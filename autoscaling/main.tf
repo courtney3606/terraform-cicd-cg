67aa139ab8d55b15b1185887b237a14f57ae4118
courtney3606 <courtney.gatlin86@gmail.com>
2022-08-29 22:24:34 +0000

    Updated 1 file.

--- autoscaling/main.tf
+++ autoscaling/main.tf
@@ -143,7 +143,7 @@ resource "aws_autoscaling_group" "cicd_asg" {
 
 
   launch_template {
-    id      = "aws_launch_template.cicdlt"
+    launch_template_id      = "aws_launch_template.cicdlt"
     version = "$Latest"
   }
 }
@@ -163,7 +163,7 @@ resource "aws_autoscaling_group" "cicd_bastion_asg" {
   health_check_type  = "EC2"
 
   launch_template {
-    id      = "aws_launch_template.cicdbastionlt"
+    launch_template_id      = "aws_launch_template.cicdbastionlt"
     version = "$Latest"
   }
 }
