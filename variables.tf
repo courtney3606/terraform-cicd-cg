186e38c66e1e8aeb45b164475c51c46fe7d51bff
courtney3606 <courtney.gatlin86@gmail.com>
2022-08-29 18:06:30 +0000

    Updated main.tf.

--- variables.tf
+++ variables.tf
@@ -1,13 +1,7 @@
 # --- root/variables.tf ---
-variable "public_sn_count" {
-  type = number
-}
-variable "private_sn_count" {
-  type = number
-}
 variable "public_cidrs" {
-  type = list(any)
+  type = list("10.0.1.0/24", "10.0.3.0/24", "10.0.5.0/24")
 }
 variable "private_cidrs" {
-  type = list(any)
+  type = list("10.0.2.0/24", "10.0.4.0/24", "10.0.6.0/24")
 }
