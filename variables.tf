773fd146a04e23109c97adc5f5ccbfe6ea3195c5
courtney3606 <courtney.gatlin86@gmail.com>
2022-08-29 18:25:46 +0000

    Update cidrs  variables.tf.

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 # --- root/variables.tf ---
 variable "public_cidrs" {
-  type = list(any)
-  default = ("10.0.1.0/24", "10.0.3.0/24", "10.0.5.0/24")
+  type = list
+  default = ["10.0.1.0/24", "10.0.3.0/24", "10.0.5.0/24"]
 }
 variable "private_cidrs" {
-  type = list(any)
-  default = ("10.0.2.0/24", "10.0.4.0/24", "10.0.6.0/24")
+  type = list
+  default = ["10.0.2.0/24", "10.0.4.0/24", "10.0.6.0/24"]
   }
