88ff3920be25a9231acfe3fa25400856ee849acb
courtney3606 <courtney.gatlin86@gmail.com>
2022-08-30 17:46:28 +0000

    Updated files.

--- autoscaling/main.tf
+++ autoscaling/main.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "sub_public" {
   count                   = var.public_sn_count
   vpc_id                  = aws_vpc.cicd_myvpc.id
   cidr_block              = var.public_cidrs[count.index]
-  availability_zone       = "data.aws_availability_zones.available.names"
+  availability_zone       = "random_shuffle.az_list"
   map_public_ip_on_launch = true
 
   tags = {
@@ -38,7 +38,7 @@ resource "aws_subnet" "sub_private" {
   count                   = var.private_sn_count
   vpc_id                  = aws_vpc.cicd_myvpc.id
   cidr_block              = var.private_cidrs[count.index]
-  availability_zone      = "data.aws_availability_zones.available.names"
+  availability_zone      = "random_shuffle.az_list"
   map_public_ip_on_launch = false
 
   tags = {
