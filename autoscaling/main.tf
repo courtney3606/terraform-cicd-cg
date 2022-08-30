5d341d5f906d60038870796184ccbb96bba340a9
courtney3606 <courtney.gatlin86@gmail.com>
2022-08-30 15:53:41 +0000

    Updated files.

--- autoscaling/main.tf
+++ autoscaling/main.tf
@@ -227,7 +227,7 @@ resource "aws_lb_target_group" "cicd_priv_tg" {
   vpc_id      = aws_vpc.cicd_myvpc.id
 }
 resource "aws_lb_listener" "cicd_lb_listener" {
-  load_balancer_arn = "aws_lb.cicd_lb[count.index]"
+  load_balancer_arn = "aws_lb.cicd_lb.arn"
   port              = 80
   protocol          = "TCP"
   default_action {
