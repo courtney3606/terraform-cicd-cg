77ca5b8cf1993ce028a25d0dadbd4b6c44bfd5e4
courtney3606 <courtney.gatlin86@gmail.com>
2022-08-30 01:32:31 +0000

    Updated 2 files.

--- variables.tf
+++ variables.tf
@@ -7,4 +7,6 @@ variable "private_cidrs" {
   type    = list(any)
   default = ["10.0.2.0/24", "10.0.4.0/24", "10.0.6.0/24"]
 }
-variable "private_sn_count" {}
+variable "private_sn_count" {
+  type = number
+}
