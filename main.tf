b47a80694c73ba8fc2a56d79e21f8602a31d2194
courtney3606 <courtney.gatlin86@gmail.com>
2022-08-30 14:34:12 +0000

    Updated 2 files.

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ module "autoscaling" {
   source           = "./autoscaling"
   public_sn_count  = 3
   private_sn_count = 3
-  private_subnets  = ["us-east-1a", "us-east-1b", "us-east-1c"]
   max_subnets      = 9
   public_cidrs     = var.public_cidrs
   private_cidrs    = var.private_cidrs
