efc9b44d9b8f7252d8b4d6641d07ed7a3278cdad
courtney3606 <courtney.gatlin86@gmail.com>
2022-08-31 15:12:36 +0000

    removed

--- autoscaling/main.tf
+++ autoscaling/main.tf
@@ -9,7 +9,7 @@ resource "random_shuffle" "az_list" {
 
 #CREATE AN AWS VPC
 resource "aws_vpc" "cicd_myvpc" {
-  cidr_block           = "10.0.140.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   instance_tenancy     = "default"
