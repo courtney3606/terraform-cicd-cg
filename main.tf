891a255a73578626b3d71f363357f1fef4b5e650
courtney3606 <courtney.gatlin86@gmail.com>
2022-08-31 01:44:05 +0000

    updated file

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ module "autoscaling" {
   public_sn_count  = 3
   private_sn_count = 3
   max_subnets      = 500
-  availability_zones          = ["var.availability_zones"]
   public_cidrs     = var.public_cidrs
   private_cidrs    = var.private_cidrs
 }
